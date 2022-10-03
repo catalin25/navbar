5105c6d1f03472d1e7199565bc5095e478ea0827
catalin25 <catalin.tudoreanu1@gmail.com>
2022-10-03 21:08:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("imaginebtn");
+const but = document.getElementById('imaginebtn');
+const scroli = document.getElementById('scroll')
+
+but.addEventListener("click", () => {
+   scroli.innerHTML = "Hello World!";
+  });
